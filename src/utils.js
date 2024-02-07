dc733e9a1bfa3a2d6bc03ee6617f7cf557b2aad7
Stephen Seo <seo.disparate@gmail.com>
2024-02-07 19:51:07 +0900

    Add more "lerp" functions to utils.js

--- src/utils.js
+++ src/utils.js
@@ -4,4 +4,15 @@ function lerp(a, b, amount) {
     return (1.0 - amount) * a + amount * b;
 }
 
-export { lerp }
+function sq_lerp(a, b, amount) {
+    let squared = amount * amount;
+    return (1.0 - squared) * a + squared * b;
+}
+
+function cube_lerp(a, b, amount) {
+    let cubed = amount - 1.0;
+    cubed = (cubed * cubed * cubed) + 1.0;
+    return (1.0 - cubed) * a + cubed * b;
+}
+
+export { lerp, sq_lerp, cube_lerp }
